31449439e9501b62bdb8c281cb1a6aafb6ab84c1
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2024-04-11 15:10:00 +0200

    Define Accommodation data model (#6)

---
+++ src/main/kotlin/com/virtualreceptionist/model/Accommodation.kt
@@ -0,0 +1,13 @@
+package com.virtualreceptionist.model
+
+data class Accommodation(
+    var ntakId: String,
+    var name: String,
+    var company: String,
+    var vatNumber: String,
+    var contact: String,
+    var headquarters: String,
+    var site: String,
+    var phone: String,
+    var email: String,
+)
