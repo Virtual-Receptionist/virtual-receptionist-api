52b1d1329438df89690cb74d78ae5760f73eac04
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2024-04-11 15:45:06 +0200

    Set up JPA (#7)

--- src/main/kotlin/com/virtualreceptionist/model/Accommodation.kt
+++ src/main/kotlin/com/virtualreceptionist/model/Accommodation.kt
@@ -1,13 +1,30 @@
 package com.virtualreceptionist.model
 
+import com.virtualreceptionist.constant.TableSchemaConstants.TABLE_NAME_ACCOMMODATION
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = TABLE_NAME_ACCOMMODATION)
 data class Accommodation(
-    var ntakId: String,
-    var name: String,
-    var company: String,
-    var vatNumber: String,
-    var contact: String,
-    var headquarters: String,
-    var site: String,
-    var phone: String,
-    var email: String,
+
+    @Id
+    var ntakId: String = "",
+
+    var name: String = "",
+
+    var company: String = "",
+
+    var vatNumber: String = "",
+
+    var contact: String = "",
+
+    var headquarters: String = "",
+
+    var site: String = "",
+
+    var phone: String = "",
+
+    var email: String = "",
 )
